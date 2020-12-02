7415341de8553ae7d0bd8792f9d544d0e2541b45
Chantal Wiebe <chantaldesiree@outlook.com>
2020-12-02 01:20:01 -0800

    Added Validation to Author and Book Models.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,12 @@
 class Author < ApplicationRecord
   has_many :books
+
+  validates :name, presence: true
+
+    def self.ordered_by_books
+    self.select("authors.*")
+        .select("COUNT(authors.id) as book_count")
+        .left_joins(:books)
+        .group("authors.id")
+        .order("book_count DESC")
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,8 @@ class Book < ApplicationRecord
   belongs_to :OrderBook
 
   has_one_attached :image
+
+  validates :title, :isbn, :published_year, presence: true
+  validates :price, presence: true, numericality: true
+  validates :rating, presence: true, numericality: true
 end
