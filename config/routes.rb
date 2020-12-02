bb647e28717dfa748f01932daa04031e76bbdaa5
Chantal Wiebe <chantaldesiree@outlook.com>
2020-12-02 01:16:32 -0800

    Added RESTful Routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'cart/index'
-  get 'cart/create'
-  get 'cart/destroy'
-  get 'book/index'
-  get 'book/show'
-  get 'book/search'
-  get 'author/index'
-  get 'author/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :cart, only: %i[index create destroy]
+  resources :authors, only: %i[index show]
+  resources :books, only: %i[index show search] do
+    collection do
+      get "search"
+    end
+  end
+
+  root "home#index"
 end
