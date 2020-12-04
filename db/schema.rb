235a4ccd49814e1b504b4120a4fa542d0b41fbd7
Chantal Wiebe <chantaldesiree@outlook.com>
2020-12-04 17:44:55 -0600

    Recreated Order and OrderBook Models.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_032311) do
+ActiveRecord::Schema.define(version: 2020_12_04_210532) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -63,6 +63,15 @@ ActiveRecord::Schema.define(version: 2020_12_04_032311) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "book_genres", force: :cascade do |t|
+    t.integer "book_id", null: false
+    t.integer "genre_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_book_genres_on_book_id"
+    t.index ["genre_id"], name: "index_book_genres_on_genre_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -130,6 +139,8 @@ ActiveRecord::Schema.define(version: 2020_12_04_032311) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "book_genres", "books"
+  add_foreign_key "book_genres", "genres"
   add_foreign_key "books", "authors"
   add_foreign_key "books", "genres"
 end
--- test/models/orderbook_test.rb
+++ test/models/orderbook_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class OrderTest < ActiveSupport::TestCase
+class OrderbookTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
